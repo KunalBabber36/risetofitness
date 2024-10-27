666152b013051930eea66c9dd2e8d2c2af6913d9
KunalBabber36 <rntc0823029@nttf.co.in>
2024-10-27 17:20:47 +0530

    Initial commit

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -33,7 +33,7 @@ app.get('/', (req, res) => {
 
 
 // MongoDB connection
-mongoose.connect('mongodb://localhost:27017/combinedDB')
+mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('MongoDB connection error:', err));
 
